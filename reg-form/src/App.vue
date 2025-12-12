f69fdc26dbc8cf0cd1fc620852a9d7dae0a495ed
vighgreta <gretu.vigh99@gmail.com>
2025-12-12 09:37:04 +0100

    Add files via upload
    Órai Feladat - 2025.12.12. - 9:36

---
+++ reg-form/src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="app">
+    <h1>Regisztrációs űrlap</h1>
+    <RegisterForm />
+  </div>
+</template>
+
+<script setup>
+import RegisterForm from './components/RegisterForm.vue'
+</script>
